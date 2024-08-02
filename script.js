b8f20720f835e530d72724c820ca23883e3f4b37
utopiaemse <utopiaemse@gmail.com>
2024-08-03 00:05:32 +0330

    fix bugs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
@@ -7,5 +8,21 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.message').textContent = `⛔ No number!`;
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = `🥳 Correct Number`;
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = `📈 Too High!`;
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = `⛔ GAME OVER!`;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = `📉 Too Low!`;
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = `⛔ GAME OVER!`;
+    }
   }
 });
