1f24b0f48a6ca9a495e02cadbbec485c7aead84c
utopiaemse <utopiaemse@gmail.com>
2024-08-02 23:05:27 +0330

    fix code

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 'use strict';
-
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = `🥳 Correct Number`;
-// document.querySelector('.score').textContent = 10;
-// document.querySelector('.number').textContent = 35;
-// document.querySelector('.guess').value = 45;
-// console.log(document.querySelector('.guess').value);
-
-// document.querySelector('.check').addEventListener('click', () => {
-//   console.log(document.querySelector('.guess').value);
-// });
-const random = Math.floor(Math.random() * 20);
-console.log(random);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
   if (!guess) {
     document.querySelector('.message').textContent = `⛔ No number!`;
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = `🥳 Correct Number`;
   }
 });
