0fcf1ac05b8a81ab493e4432649e70d0da370924
utopiaemse <utopiaemse@gmail.com>
2024-08-14 00:24:09 +0330

    fix bug

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 'use strict';
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+// document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
     document.querySelector('.message').textContent = `⛔ No number!`;
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = `🥳 Correct Number`;
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
@@ -29,4 +31,15 @@ document.querySelector('.check').addEventListener('click', () => {
       document.querySelector('.score').textContent = `0`;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.message').textContent = `Start guessing...`;
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
 });
