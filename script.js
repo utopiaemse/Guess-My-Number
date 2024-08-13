5a37df0ec731a814969e2396d5d1a5f3b87666ec
utopiaemse <utopiaemse@gmail.com>
2024-08-14 01:25:31 +0330

    complete coding

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 // document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
@@ -12,6 +13,10 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = `📈 Too High!`;
